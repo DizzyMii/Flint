7fcfc5578c2afbacf58f257ffdf7c6318384840b
DizzyMii <kadeheglin@gmail.com>
2026-04-22 08:58:43 -0600

    feat(landlord): standardTools(workDir) barrel

---
+++ packages/landlord/src/tools/index.ts
@@ -0,0 +1,16 @@
+export { fileReadTool, fileWriteTool } from './file.ts';
+export { bashTool } from './bash.ts';
+export { webFetchTool } from './web.ts';
+import type { Tool } from 'flint';
+import { fileReadTool, fileWriteTool } from './file.ts';
+import { bashTool } from './bash.ts';
+import { webFetchTool } from './web.ts';
+
+export function standardTools(workDir: string): Tool[] {
+  return [
+    fileReadTool(workDir),
+    fileWriteTool(workDir),
+    bashTool(workDir),
+    webFetchTool(workDir),
+  ];
+}
